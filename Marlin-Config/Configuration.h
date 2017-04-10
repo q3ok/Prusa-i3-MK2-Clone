fe330a46d300c247c8a42a68666059c12f8e9a37
q3ok <kkuczok@gmail.com>
2017-04-10 19:58:07 +0200

    Modified the configuration and added arduino washer

--- Marlin-Config/Configuration.h
+++ Marlin-Config/Configuration.h
@@ -523,7 +523,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 805, 102 } /* dla Z poczatkowo bylo policzone 202, ale po zmianie na 90% w sofcie drukowalo lepiej! */
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 800, 100 } 
 
 /**
  * Default Max Feed Rate (mm/s)
